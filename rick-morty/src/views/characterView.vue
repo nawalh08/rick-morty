8b0cae5c98ba6fce2a3b3a8d42235e8b0af2f561
Nawal <nawal.hammoudi08@gmail.com>
2023-12-20 11:17:29 +0100

    css détail characters

--- rick-morty/src/views/characterView.vue
+++ rick-morty/src/views/characterView.vue
@@ -1,20 +1,20 @@
 <script setup>
-import { provide, ref, watchEffect,  onMounted } from 'vue'
+import { provide, ref, watchEffect, onMounted } from 'vue'
 import { useRouter } from 'vue-router';
-import {useCharacterStore} from '../stores/character';
+import { useCharacterStore } from '../stores/character';
 
 let characters = ref([]);
 let id = ref("");
 
- 
+
 watchEffect(async () => {
-     const response = await fetch(`https://rickandmortyapi.com/api/character/?page=1`)
+    const response = await fetch(`https://rickandmortyapi.com/api/character/?page=1`)
 
     characters.value = await response.json();
-    console.log(characters.value) 
+    console.log(characters.value)
 
 });
-  
+
 
 
 </script>
@@ -22,7 +22,7 @@ watchEffect(async () => {
 <template>
     <div class="container text-center">
         <input @keyup.enter="id = $event.target.value" placeholder="saisir id">
-        <div class="row row-cols-4"  v-if="characters.results">
+        <div class="row row-cols-4" v-if="characters.results">
             <div v-for="character in characters.results" :key="character.id">
                 <div class="col my-2">
                     <div class="card" style="width: 18rem;">
@@ -30,8 +30,11 @@ watchEffect(async () => {
                             <RouterLink :to="`/details/${character.id}`">
                                 <img class="card-img-top" :src=character.image alt="image personnage">
                             </RouterLink>
-                            <h5 class="card-title">{{ character.name }}</h5>
-                            <p class="card-text"> <span class=" text-success">Species :</span> {{ character.species }}</p>
+                            <div class="text">
+                                <h5 class="card-title">{{ character.name }}</h5>
+                                <p class="card-text"> <span>Species :</span> {{ character.species }}</p>
+                            </div>
+
 
                         </div>
 
@@ -45,3 +48,30 @@ watchEffect(async () => {
 
     </div>
 </template>
+
+<style>
+.card {
+    filter: drop-shadow(0 0 0.75rem rgb(180, 178, 178));
+
+}
+
+img:hover {
+    -ms-transform: scale(1.1);
+    -webkit-transform: scale(1.1);
+    transform: scale(1.1);
+    border-radius: 5px;
+}
+
+.card-body {
+    background-color: rgb(245, 242, 242);
+    border-radius: 10px;
+
+}
+
+span {
+    color: rgb(53, 231, 53);
+}
+.text {
+    margin-top: 10px;
+}
+</style>
--- rick-morty/src/views/detailsView.vue
+++ rick-morty/src/views/detailsView.vue
@@ -2,7 +2,7 @@
 import { ref, watchEffect } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 
- 
+
 const route = useRoute();
 const router = useRouter();
 
@@ -11,12 +11,12 @@ const { characterID } = route.params
 const characterFound = ref('');
 watchEffect(async () => {
 
- 
-  
+
+
   const response = await fetch(`https://rickandmortyapi.com/api/character/${characterID}`)
 
   characterFound.value = await response.json();
-    console.log(characterFound) 
+  console.log(characterFound)
 
 });
 
@@ -26,61 +26,90 @@ watchEffect(async () => {
 
 if (!characterFound) {
   router.push('/not-found')
-} 
+}
 
 </script>
 
 <template>
-
   <div class="contenu">
-  <div class="card mb-3" style="width: 800px;">
-    <RouterLink :to="`/perso`">
-      <button class="btn btn-success">X</button>
-    </RouterLink>
-  <div class="row g-0">
-    <div class="col-md-4">
-      <img :src="characterFound.image" class="card-img-top my-4" alt="image-perso">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-    <h5 class="card-title"><span>Name : </span> {{ characterFound?.name }}</h5>
-    <p class="card-text"><span>Status : </span>{{ characterFound?.status  }}</p>
-    <p class="card-text"><span>Species : </span>{{characterFound?.species }}</p>
-    <p class="card-text"><span>Type : </span>{{ characterFound?.type }}</p>
-    <p class="card-text"><span>Gender : </span> {{ characterFound?.gender }}</p>
-    <p class="card-text"><span>Origin : </span>{{ characterFound.origin?.name }}</p>
-    <p class="card-text"><span>Location : </span> {{ characterFound.location?.name }}</p>
-    <p class="card-text"><span>Created : </span> {{ characterFound?.created }}</p>
+    <div class="card" style="width: 900px;">
+      <RouterLink :to="`/perso`">
+        <button class="btn">X</button>
+      </RouterLink>
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img :src="characterFound.image" class=" rounded-start" alt="image-perso">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body ">
+            <h5 class="card-title">Name :  {{ characterFound?.name }}</h5>
+            <p class="card-text"><span>Status : </span>{{ characterFound?.status }}</p>
+            <p class="card-text"><span>Species : </span>{{ characterFound?.species }}</p>
+            <p class="card-text"><span>Type : </span>{{ characterFound?.type }}</p>
+            <p class="card-text"><span>Gender : </span> {{ characterFound?.gender }}</p>
+            <p class="card-text"><span>Origin : </span>{{ characterFound.origin?.name }}</p>
+            <p class="card-text"><span>Location : </span> {{ characterFound.location?.name }}</p>
+            <p class="card-text"><span>Created : </span> {{ characterFound?.created }}</p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
-</div>
-</div>
-
 </template>
 
 <style scoped>
- .contenu {
-  background-color: rgb(229, 229, 229);
+.contenu {
+  background-color: rgb(255, 255, 255);
   display: flex;
   justify-content: center;
   align-items: center;
   margin-top: 5%;
+  margin-right: -20px;
+
 }
-/* .card {
+img:hover {
+    -ms-transform: scale(1);
+    -webkit-transform: scale(1);
+    transform: scale(1);
+    border-radius: 5px;
+}
+img {
+  border-radius: 5px;
+  height: 110%;
+  margin-top: -40px;
+  margin-left: 5px;
+  /* filter: drop-shadow(0 0 0.75rem rgb(152, 151, 151)); */
+}
+.card-title {
+font-family:Verdana, Geneva, Tahoma, sans-serif;
+font-weight: 600;
+margin-bottom: 20px;
+
+}
+
+.card {
   width: 18rem;
+  filter: drop-shadow(0.75 0.75 0.75rem rgb(158, 158, 158));
+  background-color: rgb(245, 242, 242);
+  border: transparent;
 }
 
 .card-body {
- text-align:left;
+  text-align: left;
+  margin-left: 110px;
+  margin-bottom: 20px;
+
 }
 
-.card-text > span{
+.card-text>span {
   color: rgb(53, 231, 53);
+  font-weight:500;
 }
-button {
-  margin-left: 90%;
-  margin-top : 8px;
 
-}  */
+button {
+  margin-left: 95%;
+  margin-top: 8px;
+  background-color:rgb(53, 231, 53); 
+
+}
 </style>
